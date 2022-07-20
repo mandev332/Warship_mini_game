ac9b713b274c7c149f0cec9ead5e0ce1e93ca220
MAN DEV <86733687+mandev332@users.noreply.github.com>
2022-07-20 14:19:15 +0500

    warship mini game in terminal
    Birinchi bo'lib ismingizni kiriting. So'ng poszitsiyalarga kemalarni joylang. Sizning ilk kemangiz vertikal 4 katakli, keyin gorizntal 4 katakli, so'ngra vertikal 3 va gorizontal 3 katakli bo'ladi. Shu tariqa 2 , 1 katak bo'lgunicha davom etadi. Keyingi navbat raqibingiz kompyutrniki. u ham 8 ta kemalarni joylab bo'lgach, o'yinni siz boshlang. Kompyuterning yashirin kemalariga oting. Tekkiza olmasangiz navbat raqibingizga o'tadi.
    Enter your name first. Then place the ships in positions. Your first deck will be 4 squares vertically, then 4 squares horizontally, then 3 squares vertically and 3 squares horizontally. This continues until there are 2 , 1 cells. The next turn is your opponent's computer. When he has also placed 8 ships, you start the game. Shoot at the computer's hidden ships. If you can't touch it, it's your opponent's turn.

---
+++ game.py
@@ -0,0 +1,52 @@
+from time import sleep as slp
+from random import randint as r
+import os as o
+from war_lib import *
+
+
+def start():
+    # player1 = input("pl1: ")
+    player1 = 'Jamshid'
+    player2 = 'KOM'
+    shoot = []
+
+    game(arr1, player1)
+    game_com(arr2)
+    e = 1
+    while(1):
+        slp(2)
+        if e % 2:
+            show2(arr2, arr1)
+            print(player1, '🎯:')
+            try:
+                p = int(input(">>> "))
+                if p > 90 or p < 1:
+                    continue
+                if not (shot(arr2, p)):
+                    show2(arr2, arr1)
+                    e += 1
+            except:
+                print("xatolik tekshiring! va qayta yozing!")
+                continue
+
+        else:
+            show2(arr1, arr2)
+            slp(2)
+            print(player2, '🎯:')
+            try:
+                p = r(1, 90)
+                if p in shoot:
+                    continue
+                if not (shot(arr1, p)):
+                    show2(arr1, arr2)
+                    e += 1
+                shoot.append(p)
+            except:
+                print("xatolik tekshiring! va qayta yozing!")
+                continue
+        if check(arr1) or check(arr2):
+            break
+
+
+start()
+show(arr1, arr2)
---
+++ war_lib.py
@@ -0,0 +1,198 @@
+
+arr1 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+]
+
+arr2 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+]
+
+
+def show(lis):
+    o.system('clear')
+    c = 1
+    for i in lis:
+        for a in range(len(i)):
+            print('\033[94m+----', end='')
+        print('+')
+        for j in i:
+            if j == ' ':
+                print(f'| \033[93m{c:2d} \033[94m', end='')
+            elif j == '0':
+                print(f'|    ', end='')
+            elif j == '*':
+                print('| \033[91m** \033[94m', end='')
+            else:
+                print(f'| \033[92m{j} \033[94m', end='')
+            c += 1
+
+        print(f'|')
+    for a in range(len(i)):
+        print('+----', end='')
+    print('+')
+
+
+def show2(lis, lis2):
+    o.system('clear')
+    c = 1
+    for i in lis:
+        for a in range(len(i)):
+            print('\033[94m+----', end='')
+        print('+')
+        for j in i:
+            if j == '*':
+                print('| 💥 \033[94m', end='')
+            elif j == '0':
+                print(f'|    ', end='')
+            else:
+                print(f'| \033[93m{c:2d} \033[94m', end='')
+            c += 1
+
+        print(f'|')
+    for a in range(len(i)):
+        print('+----', end='')
+    print('+')
+    print()
+    l = 1
+    for i in lis2:
+        for a in range(len(i)):
+            print('\033[94m+----', end='')
+        print('+')
+        for j in i:
+            if j == '*':
+                print('| 💥 \033[94m', end='')
+            elif j == '0':
+                print(f'|    ', end='')
+            else:
+                print(f'| \033[93m{l:2d} \033[94m', end='')
+            l += 1
+
+        print(f'|')
+    for a in range(len(i)):
+        print('+----', end='')
+    print('+')
+
+
+def insert(lis, P, H):
+    q = (P-1) % len(lis[0])
+    u = (P-1)//len(lis[0])
+    if H == "A":
+        if not lis[u+3]:
+            return 0
+        for i in range(4):
+            lis[u+i][q] = '🛳 '
+    elif H == "B":
+        if not lis[u][q+3]:
+            return 0
+        for i in range(4):
+            lis[u][q+i] = '⛴ '
+    elif H == "C":
+        if not lis[u+2]:
+            return 0
+        for i in range(3):
+            lis[u+i][q] = '🚢'
+    elif H == "D":
+        if not lis[u][q+2]:
+            return 0
+        for i in range(3):
+            lis[u][q+i] = '🚤'
+    elif H == "E":
+        if not lis[u+1]:
+            return 0
+        for i in range(2):
+            lis[u+i][q] = '🛥 '
+    elif H == "F":
+        if not lis[u][q+1]:
+            return 0
+        for i in range(2):
+            lis[u][q+i] = '⛵️'
+    elif H == "I":
+        lis[u][q] = '🛶'
+    elif H == "G":
+        lis[u][q] = '🛶'
+    show(lis)
+
+
+def check_com(lis):
+    count = 0
+    for i in lis:
+        for j in i:
+            if j != " ":
+                count += 1
+    if count == 20:
+        return 0
+    for i in lis:
+        for j in range(len(i)):
+            if i[j] != " ":
+                i[j] = ' '
+    return 1
+
+
+def game_com(lis):
+    kemalar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I']
+    s = 0
+    while(s != 8):
+        try:
+            p = r(1, 90)
+            if p > 90 or p < 1:
+                continue
+            insert(lis, p, kemalar[s])
+            s += 1
+        except:
+            continue
+    if check_com(lis):
+        game_com(lis)
+
+
+def game(lis, player):
+    kemalar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I']
+    show(lis)
+    s = 0
+    while(s != 8):
+        print(player, ':')
+        try:
+            p = int(input(">>> "))
+            if p > 90 or p < 1:
+                continue
+            insert(lis, p, kemalar[s])
+            s += 1
+        except:
+            print("xatolik tekshiring! va qayta yozing!")
+            continue
+
+
+def shot(lis, p):
+    q = (p-1) % len(lis[0])
+    u = (p-1)//len(lis[0])
+    if lis[u][q] != " " and lis[u][q] != "0":
+        lis[u][q] = "*"
+        return 1
+    lis[u][q] = "0"
+    return 0
+
+
+def check(lis):
+    count = 0
+    for i in lis:
+        for j in i:
+            if j == "*":
+                count += 1
+    if count == 20:
+        return 1
+    return 0
